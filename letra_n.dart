b567464f793a941bc84c6dccbd78573b4811174b
Isis França <166769666+FrancaIsis@users.noreply.github.com>
2025-10-20 21:32:45 -0300

    letra n

---
+++ letra_n.dart
@@ -0,0 +1,26 @@
+// Faça um programa para encontrar o dobro de um número caso ele seja positivo e
+// o seu triplo caso seja negativo, imprimindo o resultado.
+import 'dart:io';
+
+void main() {
+  print(' Dobro ou Triplo');
+  print('-' * 70);
+
+  stdout.write("Digite um número: ");
+  int? num1 = int.tryParse(stdin.readLineSync() ?? '');
+
+  if (num1 != null) {
+    if (num1 > 0) {
+      int resultado = num1 * 2;
+      print("O dobro de $num1 é $resultado!");
+    } else if (num1 < 0) {
+      int resultado = num1 * 3;
+      print("O triplo de $num1 é $resultado!");
+    } else {
+      print('O número informado é zero.');
+    }
+  } else {
+    print("Informe números válidos!");
+    return;
+  }
+}
